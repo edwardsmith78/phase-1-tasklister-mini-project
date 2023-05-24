0b65ddda6fbcd667b4ec0793b8187dbc29b4d96a
Edward Smith <esmith78@gmail.com>
2023-05-24 14:11:40 -0400

    added eventlisteners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+    const form = document.body.querySelector("form#create-task-form")
+    form.addEventListener("submit",submitToDo);
 });
+
+function submitToDo(e) {
+  e.preventDefault();
+  const uList = document.body.querySelector("ul#tasks");
+  const list = document.createElement("li");
+  const input = document.querySelector("input#new-task-description").value;
+  list.textContent = input;
+  uList.appendChild(list);
+}
